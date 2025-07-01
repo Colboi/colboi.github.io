a338bb36fe1f3b3c78e306f8cf6a7e0b92ae0381
Colboi <1016886725@qq.com>
2025-07-01 13:40:11 +0800

    nextconfig fix

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/colboi.github.io',
-  assetPrefix: '/colboi.github.io/',
+  basePath: isProd ? '/colboi.github.io' : '',
+  assetPrefix: isProd ? '/colboi.github.io/' : '',
 };
 
 export default nextConfig;
